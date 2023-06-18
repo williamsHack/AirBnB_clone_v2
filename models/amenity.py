a7ca789111f80b2b0566cbd493d1b46fa9539cf0
williamsHack <williamyawdickson1998@gmail.com>
2023-06-18 12:13:42 +0000

    Added many-to-many relationship with places

--- models/amenity.py
+++ models/amenity.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
-"""Defines the Amenity class."""
-from models.base_model import Base
-from models.base_model import BaseModel
-from sqlalchemy import Column
-from sqlalchemy import String
+'''
+    Implementation of the Amenity class
+'''
+from models import BaseModel, Base
+from models import place_amenity
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """Represents an Amenity for a MySQL database.
-
-    Inherits from SQLAlchemy Base and links to the MySQL table amenities.
-
-    Attributes:
-        __tablename__ (str): The name of the MySQL table to store Amenities.
-        name (sqlalchemy String): The amenity name.
-        place_amenities (sqlalchemy relationship): Place-Amenity relationship.
-    """
+    '''
+        Implementation for the Amenities.
+    '''
     __tablename__ = "amenities"
+
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary="place_amenity",
-                                   viewonly=False)
+    place_amenities = relationship("Place", secondary=place_amenity)
